c22d22047ff93e65926a9c1aac43c9835f9cbedd
KarlHarnois <karlrivestharnois@gmail.com>
2022-02-05 20:18:47 -0500

    Create handler abstract class
    This will cleanup our handlers and allow us to share code around (like
    login and JSON parsing/serialization).

---
+++ src/lambdas/handler.ts
@@ -0,0 +1,51 @@
+import { APIGatewayProxyEvent } from "aws-lambda"
+import { Logger } from "@shared/utils"
+
+export interface Event {
+  body?: any
+}
+
+export interface Response {
+  statusCode: number
+  headers: any
+  body: any
+}
+
+export abstract class Handler {
+  readonly logger: Logger
+
+  constructor(logger: Logger) {
+    this.logger = logger
+  }
+
+  abstract processEvent(event: Event): Promise<Response>
+
+  async call(event: APIGatewayProxyEvent) {
+    this.logger.logEvent({ category: "LAMBDA_EVENT", payload: event })
+
+    try {
+      const response = await this.processEvent(this.parseEvent(event))
+      return response
+    } catch(error) {
+      this.logger.logEvent({ category: "ERROR", payload: error })
+      const errorMessage = (<Error>error).message
+      return this.response(500, { message: errorMessage })
+    }
+  }
+
+  response(status: number, body: any): Response {
+    return {
+      statusCode: status,
+      headers: {},
+      body: JSON.stringify(body)
+    }
+  }
+
+  private parseEvent(event: APIGatewayProxyEvent): Event {
+    if (event.body) {
+      return { body: JSON.parse(event.body) }
+    } else {
+      return {}
+    }
+  }
+}
