8e75b66192a657918b0908216e3d59e39c6adb41
KarlHarnois <karlrivestharnois@gmail.com>
2022-02-11 15:32:52 -0500

    Cleanup expense summary builder a bit

--- src/shared/persistence/transaction-summary-builder.ts
+++ src/shared/persistence/transaction-summary-builder.ts
@@ -5,36 +5,43 @@ export class TransactionSummaryBuilder {
     monthYear: types.MonthYear,
     transactions: types.TransactionWithExpense[]
   ) {
-    let summary: types.TransactionSummary = {
+    let summary = this.createEmptySummary(monthYear)
+    transactions.forEach((transaction) => this.associate(transaction, summary))
+    return summary
+  }
+
+  private createEmptySummary(monthYear: types.MonthYear) {
+    return {
       ...monthYear,
       centAmounts: { total: 0 }
     }
+  }
 
-    transactions.forEach((txn) => {
-      const amount = txn.centAmountWithExpenses
-      summary.centAmounts.total += amount
+  private associate(
+    transaction: types.TransactionWithExpense,
+    summary: types.TransactionSummary
+  ) {
+    const amount = transaction.centAmountWithExpenses
+    summary.centAmounts.total += amount
 
-      if (txn.category) {
-        const category = txn.category.toLocaleLowerCase()
+    if (transaction.category) {
+      const category = transaction.category.toLocaleLowerCase()
 
-        if (summary.centAmounts[category] === undefined) {
-          summary.centAmounts[category] = {}
-        }
-
-        if (txn.subcategory) {
-          const subcategory = txn.subcategory.toLowerCase()
-          const currentAmount = summary.centAmounts[category][subcategory] ?? 0
-          summary.centAmounts[category][subcategory] = currentAmount + amount
-        } else {
-          const currentAmount = summary.centAmounts[category].other ?? 0
-          summary.centAmounts[category].other = currentAmount + amount
-        }
-      } else {
-        const currentAmount = summary.centAmounts.other ?? 0
-        summary.centAmounts.other = currentAmount + amount
+      if (summary.centAmounts[category] === undefined) {
+        summary.centAmounts[category] = {}
       }
-    })
 
-    return summary
+      if (transaction.subcategory) {
+        const subcategory = transaction.subcategory.toLowerCase()
+        const currentAmount = summary.centAmounts[category][subcategory] ?? 0
+        summary.centAmounts[category][subcategory] = currentAmount + amount
+      } else {
+        const currentAmount = summary.centAmounts[category].other ?? 0
+        summary.centAmounts[category].other = currentAmount + amount
+      }
+    } else {
+      const currentAmount = summary.centAmounts.other ?? 0
+      summary.centAmounts.other = currentAmount + amount
+    }
   }
 }
